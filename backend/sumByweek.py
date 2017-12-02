8bd51f194c435c46d9e4718a7b7c04503bbd4ea9
h585wang <wang130903@gmail.com>
2017-12-02 16:40:51 -0500

    update

--- backend/sumByweek.py
+++ backend/sumByweek.py
@@ -14,22 +14,8 @@ def reformat():
             del row[1]
             writer.writerow(row)
 
-def remove_lines():
-    with open('./data/blockchain.csv', 'rb') as f_in, open("./data/blockchain1.csv", "wb") as f_out:
-        #with open('./data/verge.csv', 'rb') as f_in, open("./data/vergeFilter.csv", "wb") as f_out:
-        reader = csv.reader(f_in, delimiter=",")
-        writer = csv.writer(f_out, delimiter=",")
-        keywords = ['blockchain', 'crypto', 'cryptocurrency', 'cryptocurrencies', 'bitcoin', 'btc', 'eth', 'ethereum', 'ether', 'ltc', 'litecoin', 'ripple', 'xrp'
-        'bcc']
-        for row in reader:
-            if row[1] in keywords:
-                writer.writerow(row)
-                #entryNum+= 1
-                #totalSum = totalSum+row[2]
-            else:
-                continue
 def countKey():
-    with open('./data/blockchain1.csv', 'rb') as f_in, open("./data/blockchainFinal.csv", "wb") as f_out:
+    with open('./data/websites/Edited/Block_chain_timeOrdered.csv', 'rb') as f_in, open("./data/websites/Edited/block_chainFinal.csv", "wb") as f_out:
         reader = csv.reader(f_in, delimiter=",")
         writer = csv.writer(f_out, delimiter=",")
         sumValue = 0
@@ -37,19 +23,45 @@ def countKey():
         weeklyCount =0
         for row in reader:
             date= row[0];
-            year,month,day = date.split("-")
+            year,month,day = date.split("/")
             week = datetime.date(int(year), int(month),int( day)).strftime("%V")
-            print(week)
+            #print(week)
             if week == Preweek:
                 sumValue = sumValue+float(row[2])
                 weeklyCount+= 1
             else:
-                writer.writerow((week,"bitcoin",sumValue**(1.2)))
-                sumValue=float(row[2])
-                Preweek=week
-                weeklyCount=0
+                if sumValue<=0:
+                    writer.writerow((year,week,"Cryptocurrency",0,weeklyCount))
+                    sumValue=float(row[2])
+                    Preweek=week
+                    weeklyCount=1
+                else:
+                    writer.writerow((year,week,"Cryptocurrency",(int)((sumValue*100)/(weeklyCount**(0.8))),weeklyCount))
+                    sumValue=float(row[2])
+                    Preweek=week
+                    weeklyCount=1
+
+def averageWeek():
+    with open('./data/websites/Edited/block_chainFinal.csv', 'rb1') as f_in1, open("./data/websites/Edited/block_chainFinal2.csv", "wb1") as f_out1:
+        reader1= csv.reader(f_in1, delimiter=",")
+        writer1 = csv.writer(f_out1, delimiter=",")
+        preSum = 0
+        #weekNum = 52
+        #year = 2013
+        max =0
+        for row in reader1:
+            value = int (row[3])
+            if value>=max:
+                max=value
+                print("hello")
+        print(max)
+    with open('./data/websites/Edited/block_chainFinal.csv', 'rb1') as f_in1, open("./data/websites/Edited/block_chainFinal2.csv", "wb1") as f_out1:
+        reader1 = csv.reader(f_in1, delimiter=",")
+        writer1 = csv.writer(f_out1, delimiter=",")
+        for row in reader1:
+                writer1.writerow((row[0],row[1],"Cryptocurrency",(float(row[3])/float(max))*100.0,row[4]))
 
 if __name__ == '__main__':
     # reformat()
-    remove_lines()
     countKey()
+    averageWeek()
