03c395146d13a1b30d8def87edb3d29b7d541593
justwong99 <mashimar@live.ca>
2017-12-02 17:52:35 -0500

    scroller not working

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react'
 import './Header.css';
+import Scroll from 'react-scroll'; // Imports all Mixins
+import {scroller} from 'react-scroll';
+
+
 
 class Header extends Component {
+	scrollToBottom() {
+    Scroll.scrollToBottom();
+  }
   render() {
     return (
+    	<div>
       <div className="header">
         <h1>Trendcast</h1>
         <p>Predicting the next big trends in AI and Blockchain</p>
         <div className="buttons">
-          <Button inverted color='olive' className="header-button">Algorithm</Button>
+          <Button inverted color='olive' className="header-button" >Algorithm</Button>
+          <a onClick="scrollToBottom();">Scroll to 100px from the top</a>
+
           <Button inverted className="header-button">Categories</Button>
+        </div>
+      </div>
+     <div className="algorithm">
+    
         </div>
       </div>
     );
