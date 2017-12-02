f2b4c94d6c8c637339c5864b747ebcc4bf15169d
Edwin Zhang <edwinzhang64@gmail.com>
2017-12-02 00:47:44 -0500

    Add chart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Navibar from './components/navbar/navbar.js'
 import Header from './components/Header/Header.js'
 import Footer from './components/footer/Footer.js'
+import Blockchain from './components/Blockchain/Blockchain.js'
+
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
         <Navibar/>
         <div className="main">
           <Header />
+          <Blockchain />
         </div>
         <Footer />
       </div>
