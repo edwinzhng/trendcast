7c5f33cb0ddc45467c71629bf4f189c38b788d25
justwong99 <mashimar@live.ca>
2017-12-01 22:21:26 -0500

    navbar working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Navibar from './components/navbar/navbar.js'
+import { Button } from 'reactstrap';
 import Header from './components/Header/Header.js'
 
 class App extends Component {
   render() {
     return (
       <div>
+      <Navibar/>
         <Header />
       </div>
     );
