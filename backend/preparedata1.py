154c4016c378f310d96f1b6aaf7eb0cd7914b37e
陆桁 <luheng.leo@gmail.com>
2017-12-02 05:15:54 -0500

    minor bug fixed in preparedata1.py version 1.0

--- backend/preparedata1.py
+++ backend/preparedata1.py
@@ -22,7 +22,7 @@ def filter(f_in, f_out, keyword):
                 continue
 
 def remove_lines1():
-    with open('./data/websites/verge2.csv', 'rb') as f_in1, open ('./data/websites/ai.csv', 'rb') as f_in2, open ('./data/websites/blockchain.csv', 'rb') as f_in3, open ('./data/websites/cloud.csv', 'rb') as f_in4, open ('./data/websites/engadget.csv', 'rb') as f_in5, open ('./data/websites/iot.csv', 'rb') as f_in6, open ('./data/websites/techcrunch.csv', 'rb') as f_in7, open ('./data/websites/verge.csv', 'rb') as f_in8, open("./data/websites/verge2.csv", "rb") as f_in9, open ('./data/websites/wired.csv', 'rb') as f_in10, open ('./data/websites/Edited/Artifitial_Intelligence.csv', 'wb') as f_out1:
+    with open('./data/websites/verge2.csv', 'rb') as f_in1, open ('./data/websites/ai.csv', 'rb') as f_in2, open ('./data/websites/blockchain.csv', 'rb') as f_in3, open ('./data/websites/cloud.csv', 'rb') as f_in4, open ('./data/websites/engadget.csv', 'rb') as f_in5, open ('./data/websites/iot.csv', 'rb') as f_in6, open ('./data/websites/techcrunch.csv', 'rb') as f_in7, open ('./data/websites/verge.csv', 'rb') as f_in8, open ('./data/websites/wired.csv', 'rb') as f_in9, open ('./data/websites/Edited/Artifitial_Intelligence.csv', 'wb') as f_out1:
 
         #need more keywords
         keyword_ai = ['ai', 'deep', 'learning', 'intelligence', 'artifitial', 'smart']
@@ -36,10 +36,9 @@ def remove_lines1():
         filter(f_in7, f_out1, keyword_ai)
         filter(f_in8, f_out1, keyword_ai)
         filter(f_in9, f_out1, keyword_ai)
-        filter(f_in10, f_out1, keyword_ai)
 
 def remove_lines2():
-    with open('./data/websites/verge2.csv', 'rb') as f_in1, open ('./data/websites/ai.csv', 'rb') as f_in2, open ('./data/websites/blockchain.csv', 'rb') as f_in3, open ('./data/websites/cloud.csv', 'rb') as f_in4, open ('./data/websites/engadget.csv', 'rb') as f_in5, open ('./data/websites/iot.csv', 'rb') as f_in6, open ('./data/websites/techcrunch.csv', 'rb') as f_in7, open ('./data/websites/verge.csv', 'rb') as f_in8, open("./data/websites/verge2.csv", "rb") as f_in9, open ('./data/websites/wired.csv', 'rb') as f_in10, open ('./data/websites/Edited/Block_chain.csv', 'wb') as f_out2:
+    with open('./data/websites/verge2.csv', 'rb') as f_in1, open ('./data/websites/ai.csv', 'rb') as f_in2, open ('./data/websites/blockchain.csv', 'rb') as f_in3, open ('./data/websites/cloud.csv', 'rb') as f_in4, open ('./data/websites/engadget.csv', 'rb') as f_in5, open ('./data/websites/iot.csv', 'rb') as f_in6, open ('./data/websites/techcrunch.csv', 'rb') as f_in7, open ('./data/websites/verge.csv', 'rb') as f_in8, open ('./data/websites/wired.csv', 'rb') as f_in9, open ('./data/websites/Edited/Block_chain.csv', 'wb') as f_out2:
 
         #need more keywords
         keyword_bc = ['blockchain', 'crypto', 'cryptocurrency', 'cryptocurrencies', 'bitcoin', 'btc', 'eth', 'ethereum', 'ether', 'ltc', 'litecoin', 'ripple', 'xrp'
@@ -54,7 +53,7 @@ def remove_lines2():
         filter(f_in7, f_out2, keyword_bc)
         filter(f_in8, f_out2, keyword_bc)
         filter(f_in9, f_out2, keyword_bc)
-        filter(f_in10, f_out2, keyword_bc)
+        
 
 
 
