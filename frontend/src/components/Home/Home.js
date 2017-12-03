1e28714837ba234701fa1402a0707eb9e7a88ef7
justwong99 <mashimar@live.ca>
2017-12-02 23:25:27 -0500

    pi charts done

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -7,11 +7,11 @@ import './Home.css';
 import {Link} from 'react-router-dom'
 import data from './homedata.json'
 
-const data01 = [{name: 'Deep learning', value: 400}, {name: 'Machine Learning', value: 300},
-                  {name: 'Natural Language Processing', value: 300}, {name: 'Computer Vision', value: 200}];
-const data02 = [{name: 'Ripple', value: 0.17}, {name: 'Ethereum', value: 0.23},
-                  {name: 'Litecoin', value: 0.15}, {name: 'Bitcoin', value: 0.44},
-                  {name: 'Bitcoin Cash', value: 0.21}];
+const data01 = [{name: 'Machine Learning', value: 0.33}, {name: 'Artificial Intelligence', value: 0.29},
+                  {name: 'Biometrics', value: 0.17}, {name: 'Deep Learning', value: 0.14}, {name: 'Natural Language Processing', value: 0.04}, {name: 'Computer Vision', value: 0.03}];
+const data02 = [{name: 'Bitcoin', value: 0.81}, {name: 'Ethereum', value: 0.10},
+                   {name: 'Litecoin', value: 0.05},
+                  {name: 'Ripple', value: 0.04}];
 
 console.log(data02[0]["value"]  );
 class Home extends Component {
@@ -62,12 +62,14 @@ class Home extends Component {
           <Pie isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={130} fill="#56ab2f" label/>
           <Tooltip/>
           </PieChart>
+          <p>AI appearance frequency in articles, patents, and searches.</p>
           </Col>
           <Col xs="6"><Link to="/blockchain" className="chartlinks2">Blockchain</Link>
           <PieChart width={400} height={400} className="piegraph">
           <Pie isAnimationActive={false} data={data02} cx={200} cy={200} outerRadius={130} fill="#1e90ff" label/>
           <Tooltip/>
           </PieChart>
+          <p>Cryptocurrency appearance frequency in articles, patents, and searches.</p>
           </Col>
         </Row>
         </div>
