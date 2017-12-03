bc99b7cf8944ef33d90b59811018eb73d57895bc
justwong99 <mashimar@live.ca>
2017-12-02 20:18:16 -0500

    pie charts

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import  {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ComposedChart, Area, Bar} from 'recharts';
+import  {LineChart, Line, XAxis, YAxis, PieChart, Pie, CartesianGrid, Tooltip, Legend, ComposedChart, Area, Bar} from 'recharts';
 import { Container, Row, Col } from 'reactstrap';
 import { Popup, Divider } from 'semantic-ui-react';
 import Header from '../Header/Header.js';
@@ -21,6 +21,11 @@ const data = [
       {time: '13', blockchain: 2390, bitcoin: 3800, ethereum: 2500},
       {time: '14', blockchain: 3490, bitcoin: 4300, ethereum: 2100},
 ];
+const data01 = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
+const data02 = [{name: 'Group A', value: 2400}, {name: 'Group B', value: 4567},
+                  {name: 'Group C', value: 1398}, {name: 'Group D', value: 9800},
+                  {name: 'Group E', value: 3908}, {name: 'Group F', value: 4800}];                  
 
 class Home extends Component {
 	render () {
@@ -62,6 +67,22 @@ class Home extends Component {
         </Row>
         </div>
         <Divider />
+        <div className="trending">
+           <Row>
+          <Col xs="6" className = "aitrending">AI
+          <PieChart width={400} height={400} className="piegraph">
+          <Pie isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={160} fill="#56ab2f" label/> 
+          <Tooltip/>
+          </PieChart>
+          </Col>
+          <Col xs="6">Blockchain
+          <PieChart width={400} height={400} className="piegraph">
+          <Pie isAnimationActive={false} data={data02} cx={200} cy={200} outerRadius={160} fill="#1cb5e0" label/> 
+          <Tooltip/>
+          </PieChart>
+          </Col>
+        </Row>
+        </div>
         <div className="home-content">
         	<LineChart width={1040} height={500} data={data}
                 margin={{top: 35, right: 30, left: 20, bottom: 10}}>
